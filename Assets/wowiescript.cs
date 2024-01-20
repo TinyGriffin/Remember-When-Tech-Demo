5a8153c436d4d34023b7d97d2ec2fb59d0b547de
Helena Glowacki <113125436+helenaglow@users.noreply.github.com>
2024-01-20 10:37:26 -0600

    wowie'

---
+++ Assets/wowiescript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wowie : MonoBehaviour
+{
+    void Update() {
+        Debug.Log("wowie");
+    }
+}
